f2a22b42ac031408afa91204ecde234cc40830d9
ehab-ahma <ehab24495@gmail.com>
2021-08-29 17:42:22 +0300

    modefy the react

---
+++ hornd-beast/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Main from './component/Main'
+import Header from './component/Header'
+import Footer from './component/Footer'
+
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <Main />
+     <Footer />
+    </div>  
+  );
+}
+
+export default App;
